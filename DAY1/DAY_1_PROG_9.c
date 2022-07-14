fe1a49ac63e0133c13d2461290137007639e62d5
Blessingjenisha <108974082+Blessingjenisha@users.noreply.github.com>
2022-07-14 00:02:16 -0700

    Create DAY_1_PROG_9.c

---
+++ DAY1/DAY_1_PROG_9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the two inputs\n");
+	fflush(stdout);
+	scanf("%d%d",&a,&b);
+	printf("%d&&%d=%d\n",a,b,a&&b);
+	printf("%d||%d=%d\n",a,b,a||b);
+	printf("%d=%d\n",a,!a);
+}

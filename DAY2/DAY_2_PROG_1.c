d8b65d71b58169135141d920303a0b8de303a69b
Blessingjenisha <108974082+Blessingjenisha@users.noreply.github.com>
2022-07-14 00:07:11 -0700

    Create DAY_2_PROG_1.c

---
+++ DAY2/DAY_2_PROG_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	long int num;
+	int rem,sum=0;
+	printf("enter the mobile number\n");
+	fflush(stdout);
+	scanf("%d",&num);
+	loop:
+	if(num!=0)
+	{
+		rem=num%10;
+		sum=sum+rem;
+		num=num/10;
+		goto loop;
+	}
+	printf("%d",sum);
+}
